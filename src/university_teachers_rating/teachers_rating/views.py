d93a0a711068c3810fe28f0270e81e199bb470e2
korpusovmaxim858 <korpusovmax@gmail.com>
2023-04-25 12:36:01 +0300

    render templates

--- src/university_teachers_rating/teachers_rating/views.py
+++ src/university_teachers_rating/teachers_rating/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    cards = [{
+        'name': 'Konovalov'
+    }, {
+        'name': 'длиннаааааааяфамилия имя отчество'
+    }, {
+        'name': 'Кривой Дмитрий Александрович'
+    }, {
+        'name': 'Васеков Олень Попович'
+    }]
+
+    data = {'cards': cards}
+    return render(request, 'index.html', context=data)
