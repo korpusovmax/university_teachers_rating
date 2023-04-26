7e0cd370be782ee53aa68b8646ab7e7b1f2345e9
korpusovmaxim858 <korpusovmax@gmail.com>
2023-04-26 18:42:31 +0300

    create templates of functions

--- src/university_teachers_rating/teachers_rating/views.py
+++ src/university_teachers_rating/teachers_rating/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+
+# local backend functions
+def send_rate():
+    # TODO: отправка отзыва на проверку
+    # возможные входные данные - текст отзыва, оценка/5,
+    # айди аккаунта отправителя
+    pass
+
+def get_avg_rating():
+    # TODO: получение средней оценки препода
+    # оценка должна окруляться до десятых, а если число круглое не выводить .0
+    pass
+
+# TODO: лайк
+# TODO: дизлайк
+
+
 # Create your views here.
 
 def index(request):
