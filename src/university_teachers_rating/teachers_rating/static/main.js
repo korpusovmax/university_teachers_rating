5d03e20feecb46aa4ecaba8a1426199bfe64cd37
korpusovmaxim858 <korpusovmax@gmail.com>
2023-04-30 21:19:11 +0300

    added search function

--- src/university_teachers_rating/teachers_rating/static/main.js
+++ src/university_teachers_rating/teachers_rating/static/main.js
@@ -1,9 +1,16 @@
-var element = document.getElementsByClassName('load_more')[0];
+const load_more_btn = document.getElementsByClassName('load_more')[0];
 
-element.addEventListener("click", async function (e) {
+load_more_btn.addEventListener("click", async function (e) {
     //console.log('start')
     const response = await fetch('./api/cards');
     response.text().then(function(result) {
         document.getElementsByClassName('rates')[0].innerHTML += result;
     });
+}, false);
+
+const search_btn = document.getElementsByClassName('search_section__icon')[0];
+
+search_btn.addEventListener('click', async function(e) {
+    //TODO: search request
+    //TODO: render result on the page
 }, false);
