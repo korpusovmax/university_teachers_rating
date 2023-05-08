b2dc79102e030c6d999266e40e7f98536350b478
Hazgan <57675550+Hazgan@users.noreply.github.com>
2023-05-08 17:15:19 +0300

    Update views.py
    Добавил добавление 6 строк. Нужно сделать правильный connect

--- src/university_teachers_rating/teachers_rating/views.py
+++ src/university_teachers_rating/teachers_rating/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import psycopg2
 
 
 # local backend functions
@@ -21,15 +22,34 @@ def get_avg_rating():
 # Rest API
 class CardsView(APIView):
     def get(self, request):
-        res = [{
-                'name': 'Konovalov'
-            }, {
-                'name': 'двойная-фамилия имя отчество'
-            }, {
-                'name': 'Кривой Дмитрий Александрович'
-            }, {
-                'name': 'Васеков Олень Попович'
-            }]
+        # 7850576
+        conn = psycopg2.connect(
+            host="localhost",
+            database="mydatabase",
+            user="myusername",
+            password="7850576"
+        )
+
+        cur = conn.cursor()
+
+        with open('../university_teachers_rating/prof_views.sql', 'r') as file:
+            sql = file.read()
+
+        cur.execute('SELECT fio FROM professors_data')
+
+        # res = [{
+        #         'name': 'Konovalov'
+        #     }, {
+        #         'name': 'двойная-фамилия имя отчество'
+        #     }, {
+        #         'name': 'Кривой Дмитрий Александрович'
+        #     }, {
+        #         'name': 'Васеков Олень Попович'
+        #     }]
+        res = [{'name': row[0]} for row in cur.fetchall()[:6]]
+
+        cur.close()
+        conn.close()
         data = {'cards': res}
         # return Response({'cards': res})
         return render(request, 'card.html', context=data)
